9f52a7c2f958916377e9e470cc1298f91ed4ddb2
Rishika-Drona <rishikadrona.rd@gmail.com>
2020-12-07 15:09:49 -0500

    connection

--- connection.php
+++ connection.php
@@ -1,8 +1,13 @@
 <?php
-        $databaseHost = 'localhost';
-        $databaseName = 'test';
-        $databaseUsername = 'root';
-        $databasePassword = '';
+        // $databaseHost = 'localhost';
+        // $databaseName = 'test';
+        // $databaseUsername = 'root';
+        // $databasePassword = '';
+
+        $databaseHost = 'sql9.freemysqlhosting.net';
+        $databaseName = 'sql9380827';
+        $databaseUsername = 'sql9380827';
+        $databasePassword = 'SSxDTt3W4h';
         
         $db = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
          
