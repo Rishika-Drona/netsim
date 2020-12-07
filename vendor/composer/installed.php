058a5965126d45c91fa59f88635c10eaab9243f9
Rishika-Drona <rishikadrona.rd@gmail.com>
2020-12-07 14:33:14 -0500

    compose

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '89ff68b121c14622bc41e9e741ae2396bdb028b6',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '89ff68b121c14622bc41e9e741ae2396bdb028b6',
+    ),
+  ),
+);
